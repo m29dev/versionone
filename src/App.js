94d112d1c59db4bed6348e59744b46a2322299ac
m29dev <michalmajchrzak.it@gmail.com>
2025-05-07 22:15:09 +0200

    init config dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import ClassPage from './Pages/ClassPage'
+import HomePage from './Pages/HomePage'
+import AuthPage from './Pages/AuthPage'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/auth" element={<AuthPage />} />
+                <Route path="/class" element={<ClassPage />} />
+            </Routes>
+        </Router>
+    )
 }
 
-export default App;
+export default App
